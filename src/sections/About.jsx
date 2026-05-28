f22dd2255fc9687b284d70f4077113d506b01b59
Wong Jun Xiong <wongjunxiong10@gmail.com>
2026-05-28 12:14:52 +0800

    update portfolio

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -34,7 +34,7 @@ export default function About() {
               position: 'relative',
             }}>
               <img
-                src="/profile.jpeg"
+                src="/profile1.jpeg"
                 alt="Wong Jun Xiong"
                 style={{ width: '100%', height: '100%', objectFit: 'cover', display: 'block' }}
                 onError={e => { e.target.style.display = 'none' }}
